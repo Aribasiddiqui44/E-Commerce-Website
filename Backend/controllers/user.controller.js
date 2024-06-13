e1493a703e3d18b2ff5f57b4791b1eedb6fc918b
Khawar Khan <ma3533899@gmail.com>
2024-06-13 22:55:11 +0500

    Initialized user controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,25 @@
 const User = require('./../models/user.model.js');
 
+const getUserData = async (req, res) => {
+
+};
+
+ const postRegisterUser = async (req, res) => {
+
+ };
+
+ const putChangeUserInfo = async (req, res) => {
+
+ };
+
+ const deleteUserProfile = async (req, res) => {
+
+ };
 
 
 export {
-
+    getUserData,
+    postRegisterUser,
+    putChangeUserInfo,
+    deleteUserProfile
 };
--- Backend/routes/user.router.js
+++ Backend/routes/user.router.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
-
+const { getUserData,
+    postRegisterUser,
+    putChangeUserInfo,
+    deleteUserProfile } = require('./../controllers/user.controller.js');
+    
 const router = Router();
 
-
+router.get("/get", getUserData);
+router.post("/post", postRegisterUser);
+router.put("/put", putChangeUserInfo);
+router.delete("/delete", deleteUserProfile);
 
 module.exports = {
     router
