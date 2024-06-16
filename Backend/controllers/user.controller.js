200a7319d8b9a0cbe95d7f9542edb6839dfa487c
Khawar Khan <ma3533899@gmail.com>
2024-06-16 12:29:14 +0500

    Complete logout controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -166,6 +166,35 @@ const getUserData = async (req, res) => {
  });
 
 const logoutUser = asyncHandler( async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    );
+
+    // creating and configuring cookie
+    const options = {
+        httpOnly: true,
+        secure: true
+    };
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "User logged Out"
+        )
+    );
 
 
 });
