9e18c2a2d26c212cf191f0d13779f84156f38441
Khawar Khan <ma3533899@gmail.com>
2024-06-23 23:09:57 +0500

    complete coupon add controlle

--- Backend/controllers/cart.controller.js
+++ Backend/controllers/cart.controller.js
@@ -20,7 +20,46 @@ const getCartContents = asyncHandler( async (req, res) => {
 });
 
 const postCreateCart = async (req, res) => {
+    const { productId } = req.body;
 
+    const existedCart = await Cart.findOne({ customerId: req.user._id });
+    if ( existedCart ) {
+        throw new ApiError(
+            400,
+            "Bad Request! Cart already exist for this user."
+        )
+    };
+
+    const product = await Product.findById(productId);
+
+    const newCart = await Cart.create({
+        customerId: req.user._id,
+        productsList: [
+            {
+                productId,
+                quantity: product.quantity,
+                totalCost: product.quantity * product.price,
+                isAvailable: product.isAvailable
+            }
+        ]
+    });
+
+    const checkCart = await Cart.findById(newCart._id);
+    
+    if ( !checkCart ) {
+        throw new ApiError(
+            500,
+            "Internal Server Error! Something went wrong when creating cart on cloud"
+        )
+    };
+
+    res.status(200).json(
+        new ApiResponse(
+            200,
+            checkCart,
+            "Cart created successfully"
+        )
+    );
 };
 const patchAddOrChangeProducts = async (req, res) => {
 
